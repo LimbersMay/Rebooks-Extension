fac9ba2f5605a03b86260abf7596ef68e8153822
LimbersMay <limbermay1@gmail.com>
2024-08-05 15:07:59 -0600

    feat: Add CardComponent for displaying book information

---
+++ src/app/books/components/card/card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'books-book-card',
+  templateUrl: './card.component.html',
+  styles: ``
+})
+export class CardComponent {
+
+}

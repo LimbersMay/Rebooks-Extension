ceada733ee3471a8f76f0e7bafeeef3af957625f
LimbersMay <limbermay1@gmail.com>
2024-08-05 19:24:23 -0600

    feat: Add new book page and config page to books routing module

--- src/app/books/books-routing.module.ts
+++ src/app/books/books-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LayoutPageComponent} from "../shared/layouts/layout-page/layout-page.component";
 import {ListPageComponent} from "./pages/list-page/list-page.component";
+import {NewBookPageComponent} from "./pages/new-book-page/new-book-page.component";
+import {ConfigPageComponent} from "./pages/config-page/config-page.component";
 
 const routes: Routes = [
   {
@@ -9,6 +11,9 @@ const routes: Routes = [
     component: LayoutPageComponent,
     children: [
       { path: 'list', component: ListPageComponent},
+      { path: 'edit/:id', component: NewBookPageComponent },
+      { path: 'new-book', component: NewBookPageComponent },
+      { path: 'config', component: ConfigPageComponent },
       { path: '**', redirectTo: 'list'}
     ]
   }

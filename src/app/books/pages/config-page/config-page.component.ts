0a8826e4d61ac8e07179c465460571010b47fe92
LimbersMay <limbermay1@gmail.com>
2024-08-06 00:48:24 -0600

    refactor: Update config-page component to improve layout and add language dropdown

--- src/app/books/pages/config-page/config-page.component.ts
+++ src/app/books/pages/config-page/config-page.component.ts
@@ -1,39 +1,24 @@
-import { Component } from '@angular/core';
-import {ExcelService} from "../../services/excel-service.service";
-import {BooksService} from "../../services/books.service";
-import {FileUploadEvent, UploadEvent} from "primeng/fileupload";
+import {Component} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
+interface Language {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-config-page',
   templateUrl: './config-page.component.html',
   styles: ``
 })
-export class ConfigPageComponent {
+export class ConfigPageComponent{
 
-  data = [
-    { Name: 'John Doe', Age: 30, City: 'New York' },
-    { Name: 'Jane Smith', Age: 25, City: 'San Francisco' },
-    // Add more data as needed
-  ];
+  public languages: Language[] = [
+    {name: 'English', code: 'en'}
+  ]
 
-  public file!: File
+  public configFormGroup = new FormGroup({
+    language: new FormControl({value: 'Hello', disabled: true}),
+  })
 
-  constructor(
-    private excelService: ExcelService,
-    private booksService: BooksService
-  ) {}
-
-  exportToExcel(): void {
-    console.log(this.booksService.searchBooks('').subscribe(data => this.excelService.generateExcel(data, 'books')));
-  }
-
-  async onUpload(event: FileUploadEvent): Promise<void> {
-    this.file = event.files[0];
-
-    const arrayBuffer = await this.file.arrayBuffer();
-    const books = await this.excelService.getExcelData(arrayBuffer);
-
-    console.log(books);
-  }
 }

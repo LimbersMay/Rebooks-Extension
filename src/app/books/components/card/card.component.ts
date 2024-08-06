acb26caefa1f4fd593bbe8e512e8374200f75d70
LimbersMay <limbermay1@gmail.com>
2024-08-05 23:45:25 -0600

    refactor: Update card component to include book data and delete functionality

--- src/app/books/components/card/card.component.ts
+++ src/app/books/components/card/card.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Book} from "../../interfaces/book.interface";
+import {BooksService} from "../../services/books.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'books-book-card',
   templateUrl: './card.component.html',
   styles: ``
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+  @Input()
+  public book!: Book;
 
+  @Output()
+  public onDeleteBook = new EventEmitter<string>();
+
+  public constructor(
+    private booksService: BooksService
+  ) {}
+
+  ngOnInit() {
+    if (!this.book) throw Error('Book property is required')
+  }
+
+  onDelete() {
+    this.onDeleteBook.emit(this.book.id);
+  }
 }

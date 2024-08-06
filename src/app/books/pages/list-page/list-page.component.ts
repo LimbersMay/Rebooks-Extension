c86b1a043d2363fb0523d86d6355295f6e071e29
LimbersMay <limbermay1@gmail.com>
2024-08-05 23:45:45 -0600

    refactor: Update list-page component to include lazy loading for images

--- src/app/books/pages/list-page/list-page.component.ts
+++ src/app/books/pages/list-page/list-page.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Book} from "../../interfaces/book.interface";
+import {BooksService} from "../../services/books.service";
+import {FileUploadEvent} from "primeng/fileupload";
+import {ExcelService} from "../../services/excel-service.service";
+import {delay} from "rxjs";
 
 @Component({
-  selector: 'app-list-page',
+  selector: 'books-list-page',
   templateUrl: './list-page.component.html',
   styles: ``
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
 
+  public books: Book[] = []
+  public isSearching = false;
+  public isLoading = false;
+
+  constructor(
+    private booksService: BooksService,
+    private excelService: ExcelService
+  ) {}
+
+  ngOnInit() {
+    this.booksService.getBooks().subscribe(books => {
+      this.books = books;
+    });
+  }
+
+  deleteBook(bookId: string) {
+    this.booksService.deleteBook(bookId).subscribe(() => {
+      this.books = this.books.filter(book => book.id !== bookId);
+    })
+  }
+
+  toggleSearch() {
+    this.isSearching = !this.isSearching;
+  }
+
+  searchBooks(title: string) {
+    this.booksService.searchBooks(title).subscribe(books => {
+      this.books = books;
+    });
+  }
+
+  async uploadFile(event: FileUploadEvent): Promise<void> {
+    const file = event.files[0];
+    this.isLoading = true;
+
+    const arrayBuffer = await file.arrayBuffer();
+    this.books = await this.excelService.getExcelData(arrayBuffer);
+
+    this.booksService.addBooks(this.books)
+      .pipe(
+        delay(500)
+      ).subscribe(() => {
+      this.isLoading = false;
+    });
+  }
+
+  exportToExcel() {
+    this.excelService.generateExcel(this.books, 'books');
+  }
 }

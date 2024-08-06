df3d0d4eadfb05b9ed9da0050c64e1c4bdfb1ac7
LimbersMay <limbermay1@gmail.com>
2024-08-05 23:45:16 -0600

    feat: Add new book page and config page to books module

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -6,17 +6,24 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { LayoutPageComponent } from '../shared/layouts/layout-page/layout-page.component';
 import { CardComponent } from './components/card/card.component';
 import {PrimeNgModule} from "../prime-ng/prime-ng.module";
+import { NewBookPageComponent } from './pages/new-book-page/new-book-page.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { ConfigPageComponent } from './pages/config-page/config-page.component';
 
 @NgModule({
   declarations: [
     ListPageComponent,
     LayoutPageComponent,
-    CardComponent
+    CardComponent,
+    NewBookPageComponent,
+    ConfigPageComponent
   ],
   imports: [
     CommonModule,
     BooksRoutingModule,
-    PrimeNgModule
+    PrimeNgModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class BooksModule { }

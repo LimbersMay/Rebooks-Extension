b38caccf6ec68e4ef7bdce74d422ec4136a05953
LimbersMay <limbermay1@gmail.com>
2024-08-05 23:46:04 -0600

    refactor: Add PrimeNgModule for importing and exporting PrimeNG modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {PrimeNgModule} from "../prime-ng/prime-ng.module";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    PrimeNgModule
   ]
 })
 export class SharedModule { }

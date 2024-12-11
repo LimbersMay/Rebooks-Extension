31a4f454e9a5f3a09051609a6a0221afae240e3b
LimbersMay <limbermay1@gmail.com>
2024-12-11 17:52:13 -0600

    Importaciones optimizadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from "./shared/shared.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {ProgressBarModule} from "primeng/progressbar";
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { TranslocoRootModule } from './transloco-root.module';
